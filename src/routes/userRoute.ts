d33f8efe8ce55295b38fdf2fdaa799b209137e35
Amrit Bastakoti <amritbastakoti618@gmail.com>
2022-12-01 20:51:56 +0200

    new on packages

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,5 +1,4 @@
 import { Router, Request, Response, NextFunction } from 'express'
-import userController from '../controllers/userController'
 
 const userRoute = Router()
 userRoute.post('/register', userController.registerUser)
